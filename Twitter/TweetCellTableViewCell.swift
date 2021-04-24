9cd63c421487adc1e37d97c9d5bebfa9db855428
oliviagillam <oliviagillam@Olivias-MacBook-Pro.local>
2021-04-24 01:07:54 -0500

    mostly working, still need to implement infinite scroll and pull to refresh

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Olivia Gillam on 4/24/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
