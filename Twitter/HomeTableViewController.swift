7e08c51ade8dc104427d71b615386dacbd30af13
oliviagillam <oliviagillam@Olivias-MacBook-Pro.local>
2021-04-30 23:48:10 -0500

    unrecognized selector error

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -75,6 +75,10 @@ class HomeTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweet()
+    }
 
   
     // MARK: - Table view data source
@@ -100,6 +104,10 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data{
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool )
+        cell.tweetID = (tweetArray[indexPath.row]["id"] as! Int )
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool )
         return cell
         
     }
--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -21,9 +21,86 @@ class TweetCellTableViewCell: UITableViewCell {
     
     @IBOutlet weak var tweetContent: UILabel!
     
+    @IBOutlet weak var favButton: UIButton!
+    
+    @IBOutlet weak var retweetButton: UIButton!
+    var favorited:Bool = false
+    //var retweeted:Bool = false
+    var tweetID:Int = -1
+
+    
+    @IBAction func favTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        if(toBeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetID:tweetID, success: {
+                self.setFavorite(true)
+
+            }, failure: { (error) in
+                print("favorite did not succeed: \(error)")
+            })
+        }
+        else{
+            TwitterAPICaller.client?.unfavoriteTweet(tweetID:tweetID, success: {
+                self.setFavorite(false)
+
+            }, failure: { (error) in
+                print("unfavorite did not succeed: \(error)")
+            })
+        }
+    }
+    
+    @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetID: tweetID,  success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("Unable to retweet: \(error)")
+        })
+    }
+    func setRetweeted(_ isRetweeted:Bool){
+        if (isRetweeted){
+            if #available(iOS 13.0, *) {
+                retweetButton.setImage(UIImage(systemName: "arrowshape.turn.up.right.fill"), for:  UIControl.State.normal)
+                retweetButton.isEnabled = false
+            } else {
+                // Fallback on earlier versions
+            }
+            
+        }
+        else{
+            if #available(iOS 13.0, *) {
+                retweetButton.setImage(UIImage(systemName: "arrowshape.turn.up.right"), for:  UIControl.State.normal)
+                retweetButton.isEnabled = true
+            } else {
+                // Fallback on earlier versions
+            }
+            
+        }
+    }
+    
+    
+    func setFavorite(_ isFavorited:Bool){
+        favorited = isFavorited
+        if( favorited) {
+            if #available(iOS 13.0, *) {
+                favButton.setImage(UIImage(systemName: "shift.fill"), for:  UIControl.State.normal)
+            } else {
+                // Fallback on earlier versions
+            }
+        }
+        else{
+            if #available(iOS 13.0, *) {
+                favButton.setImage(UIImage(systemName: "shift"), for:  UIControl.State.normal)
+            } else {
+                // Fallback on earlier versions
+            }
+        }
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
+        
+        
         // Configure the view for the selected state
     }
 
